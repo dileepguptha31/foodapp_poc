1afabfc36062a80d605f66f7a89f17dc15661354
Dileep Mada <dileepguptha31@gmail.com>
2024-06-19 11:42:05 +0000

    First commit from docker local

---
+++ src/app/views/catalogueitems/catalogueitems.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogueitems',
+  standalone: true,
+  imports: [],
+  templateUrl: './catalogueitems.component.html',
+  styleUrl: './catalogueitems.component.scss'
+})
+export class CatalogueitemsComponent {
+
+}
