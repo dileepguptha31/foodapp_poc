443f016c427a2247ec92cb7e33db1283f32f41de
Sathish S <s.sathish@siemens-healthineers.com>
2024-06-23 09:21:18 +0530

    Spinner Correction and Catch Error

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, tap } from 'rxjs';
+import { Observable, catchError, tap, throwError } from 'rxjs';
 import { LoaderService } from './loader.service';
 
 @Injectable()
@@ -11,18 +11,31 @@ export class HttpService {
   getHTTP(methodName: string): Observable<any> {
     this.loaderService.loaderStart();
     return this.httpClient.get(this.serviceURL + methodName)
-      .pipe(tap(() => this.loaderService.loaderEnd()));
+      .pipe(
+        tap(() => this.loaderService.loaderEnd()),
+        catchError(() => this.errorHandler())
+      );
   }
 
   postHTTP(methodName: string, body: any): Observable<any> {
     this.loaderService.loaderStart();
     return this.httpClient.post(this.serviceURL + methodName, body)
-      .pipe(tap(() => this.loaderService.loaderEnd()));
+      .pipe(tap(() => this.loaderService.loaderEnd()),
+        catchError(() => this.errorHandler()));
   }
 
   getHTTPbyId(methodName: string, id: number): Observable<any> {
     this.loaderService.loaderStart();
     return this.httpClient.get(this.serviceURL + methodName + "/" + id)
-      .pipe(tap(() => this.loaderService.loaderEnd()));
+      .pipe(tap(() => this.loaderService.loaderEnd()),
+        catchError(() => this.errorHandler()));
   }
+
+  errorHandler(error?: HttpErrorResponse) {
+    return throwError(() => {
+      this.loaderService.loaderEnd();
+      return new Error('API Call Failed');
+    })
+  }
+
 }
