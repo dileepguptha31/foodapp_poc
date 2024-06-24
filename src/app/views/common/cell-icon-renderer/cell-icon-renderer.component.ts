8fa1355b2376e528d11d0e7c97c83ea1c7e95a27
Dileep Mada <dileepguptha31@users.noreply.github.com>
2024-06-24 22:06:28 +0530

    Removing build failure code

--- src/app/views/common/cell-icon-renderer/cell-icon-renderer.component.ts
+++ src/app/views/common/cell-icon-renderer/cell-icon-renderer.component.ts
@@ -1,42 +1,16 @@
 import { Component } from '@angular/core';
 import { faEdit } from '@fortawesome/free-regular-svg-icons';
 import { IconDefinition, faCoffee, faEraser, faStreetView } from '@fortawesome/free-solid-svg-icons';
-import { ICellRendererAngularComp } from 'ag-grid-angular'
-import { ICellRendererParams, } from 'ag-grid-community';
 
 @Component({
   selector: 'app-cell-icon-renderer',
   template: `<fa-icon [icon]="displayIcon"></fa-icon>`,
   styles: ``
 })
-export class CellIconRendererComponent implements ICellRendererAngularComp {
+export class CellIconRendererComponent  {
   private fontIcon: IconDefinition = faCoffee;
   get displayIcon(): IconDefinition {
     return this.fontIcon;
-  }
-  agInit(params: ICellRendererParams<any, any, any>): void {
-    console.log(params)
-    if (!params.colDef?.headerName) {
-      return;
-    }
-
-    switch (params.colDef?.headerName) {
-      case 'Edit':
-        this.fontIcon = faEdit;
-        break;
-      case 'Delete':
-        this.fontIcon = faEraser;
-        break;
-      case 'View':
-        this.fontIcon = faStreetView;
-        break;
-      default:
-        break;
-    }
-
-  }
-  refresh(params: ICellRendererParams<any, any, any>): boolean {
-    return true;
-  }
+  } 
 
 }
